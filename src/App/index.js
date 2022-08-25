551ab52f8fde49817c3a27a96941df1432eb74eb
mperezs06408 <mperezs06408@universidadean.edu.co>
2022-08-24 20:00:48 -0500

    Composición de componentes

--- src/App/index.js
+++ src/App/index.js
@@ -2,25 +2,71 @@ import React from 'react';
 import {AppUI} from './AppUI';
 import { TodoProvider } from '../TodoContext';
 
-const defaultTasks = [
-  {
-    text: 'Cortar cebolla',
-    completed: false
-  }, {
-    text: 'Tomar curso de intro a React',
-    completed: false
-  }, {
-    text: 'Llorar con la llorona',
-    completed: false
-  }
-];
-
 function App() {
+  const [state, setState] = React.useState('Initial state');
   return (
-    <TodoProvider>
-      <AppUI/>
-    </TodoProvider>
-  );
+    <>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+      <TodoList>
+        <TodoItems state={state} />
+      </TodoList>
+    </>
+  )
 }
+
+
+function TodoHeader({ children }) {
+  return (
+    <header>
+      {children}
+    </header>
+  )
+}
+
+
+function TodoList({ children }) {
+  return (
+    <section className='TodoList__container'>
+      {children}
+    </section >
+  )
+}
+
+function TodoCounter() {
+  return (
+    <>
+      <p>TodoCounter</p>
+    </>
+  )
+}
+
+
+function TodoSearch() {
+  return (
+    <>
+      <p>TodoSearch</p>
+    </>
+  )
+}
+
+
+function TodoItems({ state }) {
+  return (
+    <>
+      <p>{state}</p>
+    </>
+  )
+}
+
+// function App() {
+  // return (
+    // <TodoProvider>
+      {/* <AppUI/> */}
+    {/* </TodoProvider> */}
+  // );
+// }
 
 export default App;
